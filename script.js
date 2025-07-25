becbfb6c1d8cb6561b3b7014c87f9b0b4afcb86c
Eman Tariq <emantariq.1004@gmail.com>
2025-07-26 00:16:36 +0500

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let toastBox = document.getElementById("toastBox");
-let successMsg = "Successfully Submitted!";
-let errorMsg = "Please fix the error";
-let invalidMsg = "Invalid Input, Check Again";
+let successMsg = `<i class="fa-solid fa-circle-check"></i> Successfully Submitted!`;
+let errorMsg = `<i class="fa-solid fa-circle-xmark"></i> Please fix the error`;
+let invalidMsg = `<i class="fa-solid fa-exclamation"></i> Invalid Input, Check Again`;
 
 function showToast(msg) {
   let toast = document.createElement("div");
   toast.classList.add("toast");
   toast.innerHTML = msg;
   toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 3000);
+
 }
